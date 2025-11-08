58b1384c235fc2c992813b5537cf9bd5d280cef1
Aluno SENAI <Aluno@SN-345325.edu.fiesc.com.br>
2025-11-08 13:32:13 -0300

    Primeiro commit - Projeto EPI Systema

---
+++ apps/colaboradores/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Colaborador(models.Model):
+    nome = models.CharField(max_length=120)
+    cpf = models.CharField(max_length=14, unique=True)  
+    setor = models.CharField(max_length=80)
+    idade = models.PositiveIntegerField(null=True, blank=True)
+    matricula = models.CharField(max_length=20, unique=True, blank=True, null=True)
+    foto_colaborador = models.ImageField(
+        upload_to='colaboradores/fotos/',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return f"{self.nome} ({self.setor})"
